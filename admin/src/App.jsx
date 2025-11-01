9047e1a5cda6ebc0dace8cc6c83206b443566bde
ritesh harge <ritesh@example.com>
2025-11-01 12:10:53 +0530

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-   const url = 'https://foodify-mern-backend.onrender.com';
+   const url = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
   return (
     <div>
       <ToastContainer />
